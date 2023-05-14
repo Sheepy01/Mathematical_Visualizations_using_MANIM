1c6fa01fabb306ea94c619eb47084d1bcc210b50
Sheepy01 <112115729+Sheepy01@users.noreply.github.com>
2023-05-15 03:27:02 +0530

    SquareAnimation Test

--- Introduction.py
+++ Introduction.py
@@ -7,4 +7,4 @@ class SquareAnimation(Scene):
     def construct(self):
         square = Square(side_length = 5, stroke_color = RED, fill_color = BLUE, fill_opacity = 0.75)
         self.play(Create(square), run_time = 5)
-        self.Wait
+        self.wait()
