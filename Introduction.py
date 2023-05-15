0ec3707c147468976faa464185e4ba7e85eb1e13
Sheepy01 <112115729+Sheepy01@users.noreply.github.com>
2023-05-15 10:21:44 +0530

    Print statement

--- Introduction.py
+++ Introduction.py
@@ -6,5 +6,6 @@ from manim import *
 class SquareAnimation(Scene):
     def construct(self):
         square = Square(side_length = 5, stroke_color = RED, fill_color = BLUE, fill_opacity = 0.75)
+        print(square)
         self.play(Create(square), run_time = 5)
         self.wait()
